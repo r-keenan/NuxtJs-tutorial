26dd28a6a57da21641d8943c4874d6580f8be3d7
Ross Keenan <ross.keenan@protonmail.com>
2021-08-01 18:31:47 -0500

    Added in validation so only integer can be used for product search

--- pages/products/_pid/index.vue
+++ pages/products/_pid/index.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>A single product, with ID: {{ $route.params.pid }}</h1>
 </template>
+
+<script>
+export default {
+  validate(data) {
+    return /^\d+$/.test(data.params.pid);
+  }
+};
+</script>
